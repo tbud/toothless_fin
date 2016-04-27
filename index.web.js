3d636898a724960708d1aace1ff5876c83f77f78
peace0phmind <peace0phmind@gmail.com>
2016-04-27 17:35:45 +0800

    add hmr support

--- index.web.js
+++ index.web.js
@@ -5,4 +5,9 @@ import ReactDOM from 'react/lib/ReactDOM';
 
 import App from './App';
 
+if (__DEV__) {
+  const HMRClient = require('HMRClient');
+  HMRClient.enable('web', 'index.web.bundle', 'localhost', 8081);
+}
+
 ReactDOM.render(<App />, document.getElementById('root'));
