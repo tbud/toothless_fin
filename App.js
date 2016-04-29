5315af59c1f4fc713511e00f0639f5d96e269023
ewei <ewei9046des@hotmail.com>
2016-04-29 18:37:23 +0800

    add input sample

--- App.js
+++ App.js
@@ -30,11 +30,16 @@ export default class App extends Component {
                 <View style={{height:50, width:50, backgroundColor:'#ddd'}} onPress={()=> Alert.alert('Hello')}/>
                 <Text>{this.state.showText}</Text>
 
-                <Input placeholder="normal input" />
+                <Input placeholder="normal input" keyboardType selectTextOnFocus />
+                <Input placeholder="disabled input" disabled value="Disable Input"/>
+                <Input placeholder="normal error input" error value="enter error info"/>
                 <Input placeholder="password input" password value="123"/>
                 <Input placeholder="number input" keyboardType="numeric" value="123"/>
                 <Input placeholder="phone input" keyboardType="phone-pad" value="13913911339"/>
                 <Input placeholder="email input" keyboardType="email-address" value="123@ttt.com"/>
+                <Input placeholder="inline input" singleLine keyboardType="email-address"/>
+                <Input placeholder="inline disabled input" singleLine disabled keyboardType="email-address"/>
+                <Input placeholder="inline error input" singleLine error keyboardType="email-address"  value="enter error info"/>
 
                 <Image style={{width: 50, height: 50}}
                        source={{uri: 'http://facebook.github.io/react/img/logo_og.png'}}/>
@@ -59,6 +64,7 @@ export default class App extends Component {
 const styles = StyleSheet.create({
     container: {
         flex: 1,
+        height: 1000,
         // width: 250,
         // height: 230,
         // left:0,
