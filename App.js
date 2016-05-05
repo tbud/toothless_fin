45051e0452ff850b926c596d9af789f8be4554c2
ewei <ewei9046des@hotmail.com>
2016-05-05 13:32:44 +0800

    add scrollview

--- App.js
+++ App.js
@@ -7,7 +7,7 @@ import React, {
     Text,
     Input,
     Image,
-
+    ScrollView,
     Scale,
     StyleSheet,
 } from 'toothless';
@@ -27,19 +27,20 @@ export default class App extends Component {
     render() {
         return (
             <View style={styles.container}>
+                <ScrollView>
                 <View style={{height:50, width:50, backgroundColor:'#ddd'}} onPress={()=> Alert.alert('Hello')}/>
                 <Text>{this.state.showText}</Text>
 
                 <Input placeholder="normal input" keyboardType selectTextOnFocus />
-                <Input placeholder="disabled input" disabled value="Disable Input"/>
-                <Input placeholder="normal error input" error value="enter error info"/>
-                <Input placeholder="password input" password value="123"/>
-                <Input placeholder="number input" keyboardType="numeric" value="123"/>
-                <Input placeholder="phone input" keyboardType="phone-pad" value="13913911339"/>
-                <Input placeholder="email input" keyboardType="email-address" value="123@ttt.com"/>
+                <Input placeholder="disabled input" disabled defaultValue="Disable Input"/>
+                <Input placeholder="normal error input" error defaultValue="enter error info"/>
+                <Input placeholder="password input" password defaultValue="123"/>
+                <Input placeholder="number input" keyboardType="numeric" defaultValue="123"/>
+                <Input placeholder="phone input" keyboardType="phone-pad" defaultValue="13913911339"/>
+                <Input placeholder="email input" keyboardType="email-address" defaultValue="123@ttt.com"/>
                 <Input placeholder="inline input" singleLine keyboardType="email-address"/>
                 <Input placeholder="inline disabled input" singleLine disabled keyboardType="email-address"/>
-                <Input placeholder="inline error input" singleLine error keyboardType="email-address"  value="enter error info"/>
+                <Input placeholder="inline error input" singleLine error keyboardType="email-address"  defaultValue="enter error info"/>
 
                 <Image style={{width: 50, height: 50}}
                        source={{uri: 'http://facebook.github.io/react/img/logo_og.png'}}/>
@@ -52,6 +53,7 @@ export default class App extends Component {
                 <Button value='Default Block Disabled' disabled block onPress={()=> this._handleClick()}/>
                 <Button value='Primary Disabled' disabled type="primary" onPress={()=> this._handleClick()}/>
                 <Button value='Ghost Disabled' disabled type="flat" onPress={()=> this._handleClick()}/>
+                </ScrollView>
             </View>
         )
     }
